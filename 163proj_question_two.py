88b021fa67ea964d6a0aa120f55a647af5bebe7c
ShiraZur <114948219+ShiraZur@users.noreply.github.com>
2023-08-16 16:34:08 -0700

    Update 163proj_question_two.py

--- 163proj_question_two.py
+++ 163proj_question_two.py
@@ -1,4 +1,18 @@
+"""
+Shira Zur
+CSE 163
 
+Research Question 2 for group project,
+creates a map of the female athletes sent to
+2016 Olympics by country and of the male
+athletes sent to 2016 Olympics by country.
+"""
+
+import geopandas as gpd
+import matplotlib.pyplot as plt
+import plotly.express as px
+
+import loadprojdata
 
 
 
