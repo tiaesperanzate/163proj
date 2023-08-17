4ae06591e001b987c5873ced4141e387ebaeaa6d
ShiraZur <114948219+ShiraZur@users.noreply.github.com>
2023-08-16 19:55:04 -0700

    Update and rename RQ1test.py to test_file.py

--- testing data/test_file.py
+++ testing data/test_file.py
@@ -1,19 +1,30 @@
 """
 CSE 163
 
-Tests sums_on_geos in rq1.py
+Tests methods in CSE 163 project.
 """
 
 import geopandas as gpd
 
 from loadprojdata import RQ1_loading_data
 from rq1 import sums_on_geos
+from loadprojdata import RQ2_loading_data
 from cse163_utils import assert_equals
 
 
 def test_sums_on_geos(athlete_gdp_info: gpd.GeoDataFrame,
                       countries_shp: str) -> None:
     assert_equals(3, sums_on_geos(athlete_gdp_info, countries_shp))
+                        
+
+def test_RQ2_loading_data(athletes_csv: str,
+                          countries_csv: str,
+                          world_shp: str,
+                          iso_to_country_csv: str) -> None:
+    assert_equals((111, 22), RQ2_loading_data(athletes_csv,
+                                              countries_csv,
+                                              world_shp,
+                                              iso_to_country_csv).shape)
 
 
 def main():
@@ -31,6 +42,10 @@ def main():
                                         'Russian Federation'])])
 
     test_sums_on_geos(test_data, countries_shps)
+    test_RQ2_loading_data('data/athletes.csv',
+                          'data/countries.csv',
+                          'data/World_Countries_Generalized.shp',
+                          'data/all.csv')
 
 
 if __name__ == '__main__':
