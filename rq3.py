eaa3fcd06fdda6962aefc5c22ca8ba76bbde5a95
DaeunLee123 <82142774+DaeunLee123@users.noreply.github.com>
2023-08-16 20:02:49 -0700

    Update rq3.py

--- rq3.py
+++ rq3.py
@@ -11,35 +11,27 @@ won.
 """
 import pandas as pd
 import plotly.express as px
+import matplotlib.pyplot as plt
 
 
-def rq3(data: DataFrame) -> None:
+def rq3(data) -> None:
     """
-    Reads in the atheletes data to create a scatter plot 
+    Reads in the athletes data to create a scatter plot
     Returns none
     """
-    col = ['gold', 'silver', 'bronze', 'height', 'weight']
-    d = col[(col['gold'] >= 1) | (col['silver'] >= 1)
-                     | (col['bronze'] >= 1)]
-    medal_data = []
-    for index, row in d.iterrows():
-        if row['gold'] > 0:
-            medal_data.append({'Medal': 'Gold', 'Height': row['height'], 'Weight': row['weight']})
-        if row['silver'] > 0:
-            medal_data.append({'Medal': 'Silver', 'Height': row['height'], 'Weight': row['weight']})
-        if row['bronze'] > 0:
-            medal_data.append({'Medal': 'Bronze', 'Height': row['height'], 'Weight': row['weight']})
-    df = pd.DataFrame(medal_data)
-    fig = px.scatter(df, x = 'Height', y = 'Weight', color = 'Medal'
-                     title = "Olympic Athelete Measurements and Medals Won",
-                     color_discrete_map={'Gold': 'gold', 'Silver': 'silver',
-                     'Bronze': 'orange'})
+    df = data.loc[:, ['gold', 'silver', 'bronze', 'height', 'weight']]
+    df['total'] = df['gold'] + df['silver'] + df['bronze']
+    print(df)
+    fig = px.scatter(df, x='height', y='weight', color='total',
+                     title="Olympic Athelete Measurements and Medals Won")
     fig.update_xaxes(title_text='Height (m)')
     fig.update_yaxes(title_text='Weight (kg)')
+    fig.show()
+
 
 def main():
     data = pd.read_csv('athletes.csv')
-    rq3(data: DataFrame)
+    rq3(data)
 
 
 if __name__ == '__main__':
